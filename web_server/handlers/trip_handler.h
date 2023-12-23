3b3e81b53ce7d0ce31f3c58c71ec5263f6263b2b
VasiliyGoloskov <vasiliy@DESKTOP-HDRP4R4>
2023-12-23 07:58:38 +0300

    trip service

--- web_server/handlers/trip_handler.h
+++ web_server/handlers/trip_handler.h
@@ -173,7 +173,7 @@ public:
                     database::Trip trip;
                  
                     trip.host_id() =  atol(form.get("host_id").c_str());      
-                    time_t now = time(0);
+                    //time_t now = time(0);
                     
                     // В таком формате мы получаем дату. Sat Nov 18 21:34:02 2023
                     //std::string temp_value = ctime(&now);
